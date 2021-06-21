d840bb6abfc67118dc6593e3c1e132e4042b06a2
amaiacc <amaia.car@gmail.com>
2021-06-21 19:22:03 +0200

    Check that issue is not already open (in donwloaded issue list).

--- googleForm2projectIssue.sh
+++ googleForm2projectIssue.sh
@@ -21,19 +21,34 @@ wget --no-check-certificate ${url_file} -O 'project_data/BHD2021_projects.csv'
 Rscript --verbose googleForm2projectIssue.R project_data/BHD2021_projects.csv
 
 # For each new project, open a new issue in github (BHD2021?)
+
+gh issue list | grep "^[^#;]" | awk '{print $3}'> open_issues.txt # create list open issue titles --> do not resubmit if project has an open issue
+
 cd project_issues
 
 # condition on having a list_new_projects.txt file
-if -f list_new_projects.txt 
+if [ -f list_new_projects.txt ]
 then 
  while IFS=" " read -r v1 v2 remainder
   do
    id=$(echo $v1 | sed 's/"//g')
    title=$(echo $v2 | sed 's/"//g')
- #  echo "The title for ${id} is ${title}."
-   gh issue create --title ${title} --body-file ${id}.md
+   # only open issue if not listed as an open issue
+   if ! grep -q ${title} ../open_issues.txt
+    then
+     echo "The title for ${id} is ${title}."
+    # gh issue create --title ${title} --body-file ${id}.md
+    else
+     echo "Issue for project with title '${title}' is already open."
+   fi
  done < list_new_projects.txt
 fi
+
+rm list_new_projects.txt # remove file
+if grep -q PATTERN file.txt; then
+    echo found
+else
+    echo not found
 
 # remove list of new projects after creating the issues - to avoid resubmitting them
 rm list_new_projects.txt 
