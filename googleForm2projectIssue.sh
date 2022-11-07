0319ea441ef0de34aae7d2378e5f47e8cd8fc8ef
amaiacc <amaia.car@gmail.com>
2022-11-07 22:26:45 +0100

    Edited bash script to make more general. It now takes 2 arguments (currenlty hardcoded): url for file, and year (for BHD event). You can specify a target repo, so that the new projects are submitted to that target repo as issues (could clean it a bit further, but working at the moment.

--- googleForm2projectIssue.sh
+++ googleForm2projectIssue.sh
@@ -1,58 +1,80 @@
 #! /bin/bash
 
 # script to download google form and format it into a md file that can be submitted as a git issue
-# Amaia Carrión-Castillo, 2021.06.18
+# Amaia Carrión-Castillo, 2021.06.18, edited 2022.11.07
 
 ## specify the url for the target google form as argument
 url_file=$1
+year=$2
+url_file=https://docs.google.com/spreadsheets/d/1CTYnf0aFwSdK2Ph2x-S9tITyOr3BeG9k5QtbMG91NXQ/export?format=csv
+year=2022
 
 # define directories
+bhd_dir=/home/ina/Escritorio/amaia/BHD/
+
+# clone git project where issues should be submited
+#--------------------------------------------------------
+## clone amaia's BHD repo for testing purposes
+# git clone https://github.com/amaiacc/BHD_issues4global
+#--------------------------------------------------------
+# clone global brainhack repo
+# cd ${bhd_dir}
+# git clone https://github.com/brainhackorg/global${year}.git
+# cd ${bhd_dir}/global${year}
+#--------------------------------------------------------
+# define target repo, to get issues from:
+target_repo=BHD_issues4global
+target_repo=global${year}
+
+
+cd ${bhd_dir}
 mkdir -p project_data project_issues
 
 # define target format, from git an example git issue --> in templates
 ## https://github.com/brainhackorg/global2020/issues/new?assignees=&labels=project&template=project-submission-template.md&title=
 
-
 # get google form as csv output
-
-wget --no-check-certificate ${url_file} -O 'project_data/BHD2021_projects.csv' 
+wget --no-check-certificate ${url_file} -O project_data/BHD${year}_projects.csv
 
 # replace all LF that are not preceded by CR with a space (i.e. keep CRLF)
 ## from: https://stackoverflow.com/questions/50737164/how-to-use-sed-to-substitute-lf-with-space-but-not-crlf
-awk 'BEGIN{RS=ORS="\r\n"}/\n/{sub(/\n/,"")}1' 'project_data/BHD2021_projects.csv' > 'project_data/BHD2021_projects_edited.csv'
-awk '!/\r$/{printf "%s",$0;next}1' 'project_data/BHD2021_projects.csv' > 'project_data/BHD2021_projects_edited.csv'
+awk 'BEGIN{RS=ORS="\r\n"}/\n/{sub(/\n/,"")}1' project_data/BHD${year}_projects.csv > project_data/BHD${year}_projects_edited.csv
+awk '!/\r$/{printf "%s",$0;next}1' project_data/BHD${year}_projects.csv > project_data/BHD${year}_projects_edited.csv
 # add \n at the end of the file, if not present
 ## from: https://unix.stackexchange.com/questions/31947/how-to-add-a-newline-to-the-end-of-a-file
-sed -i -e '$a\' 'project_data/BHD2021_projects_edited.csv'
+sed -i -e '$a\' project_data/BHD${year}_projects_edited.csv
 
 # make sure it's in unix format, otherwise problems when reading with R if there are newline and other system specific characters
-dos2unix ./project_data/BHD2021_projects_edited.csv
+dos2unix ./project_data/BHD${year}_projects_edited.csv
 
 # run this script to create an adequatelly formatted markdown file for each project
-Rscript --verbose googleForm2projectIssue.R ./project_data/BHD2021_projects_edited.csv
+Rscript --verbose ${bhd_dir}/BHD_issues4global/googleForm2projectIssue.R ./project_data/BHD${year}_projects_edited.csv
 
-# For each new project, open a new issue in github (BHD2021?)
-gh issue list | grep "^[^#;]" | awk '{print $3}'> open_issues.txt # create list open issue titles --> do not resubmit if project has an open issue
 
-cd project_issues
+# get issues in target repo
+cd ${bhd_dir}/${target_repo} # cd to global repo if issues should be submitted there.. just trying it here first
 
+# get list of current issues in repo
+gh issue list | grep "^[^#;]" | awk '{print $3}'> open_issues_BH${year}.txt # create list open issue titles --> do not resubmit if project has an open issue
+
+# For each new project, open a new issue in github
 # condition on having a list_new_projects.txt file
-if [ -f list_new_projects.txt ]
-then 
+if [ -f ${bhd_dir}/project_issues/list_new_projects.txt ]
+ then 
  while IFS=" " read -r v1 v2 remainder
   do
    id=$(echo $v1 | sed 's/"//g')
-   title=$(echo $v2 | sed 's/"//g')
+   title=$(echo $v2 | sed 's/"//g' | awk -F":" '{print $1}' )
    # only open issue if not listed as an open issue
-   if ! grep -q ${title} ../open_issues.txt
+   if ! grep -q ${title} open_issues_BH${year}.txt
     then
      echo "The title for ${id} is ${title}."
-     gh issue create --title ${title} --body-file ${id}.md
+    #gh issue create --title ${title} --body-file ${bhd_dir}/project_issues/${id}.md
     else
      echo "Issue for project with title '${title}' is already open."
    fi
- done < list_new_projects.txt
+ done < ${bhd_dir}/project_issues/list_new_projects.txt
  # remove list of new projects after creating the issues - to avoid resubmitting them
- rm list_new_projects.txt # remove file 
+ #rm ${bhd_dir}/BHD_issues4global/project_issues/list_new_projects.txt # remove file 
 fi
 
